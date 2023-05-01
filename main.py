cb56fa589b2f04109a91fc41d26135d019751c68
Ewy <EwyBoy@gmail.com>
2023-05-01 17:19:19 +0200

    Init()

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+@bot.command()
+async def ping(ctx):
+    """Check the bot's latency."""
+    await ctx.send(f"Pong! Latency: {round(bot.latency * 1000)}ms")
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.custom,
+            name="Chilling in the fridge 🧊"
+        )
+    )
+    print(f"Bot connected as {bot.user.name}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    bot.run(sys.argv[1])
